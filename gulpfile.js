2026b0f7376161d4812f8f2c0fc835e379ca622c
Tom Mazo <tom.mazo.tm@gmail.com>
2016-10-26 01:32:49 +0200

    first commit of this branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var     gulp        = require('gulp'),
         $           = require('gulp-load-plugins')(),
-        browserSync = require('browser-sync').create();
+        browserSync = require('browser-sync').create(),
+        path        = require('path'),
+        fs          = require('fs');
 
 var sassPaths = [
   'bower_components/foundation-sites/scss',
@@ -24,7 +26,7 @@ gulp.task('sass', function() {
 });
 
 
-gulp.task('deploy', function() {
+gulp.task('deploy', ['build'], function() {
   console.log('deploy task running');
   return gulp.src('./dist/**/*')
     .pipe($.ghPages());
@@ -44,9 +46,18 @@ gulp.task('scripts', function() {
   // minify/uglify
 });
 
-gulp.task('markup', function() {
+gulp.task('get-content', function() {
+  return gulp.src('src/data/partials/*.json')
+    .pipe($.mergeJson('bundle.json'))
+    .pipe(gulp.dest('src/data/'));
+});
+
+gulp.task('markup', ['get-content'], function() {
   return gulp.src('src/markup/index.pug')
     .pipe($.pugLint())
+    .pipe($.data(function(file) {
+      return require('./src/data/bundle.json');
+    }))
     .pipe($.pug({
       pretty: true
     }))
